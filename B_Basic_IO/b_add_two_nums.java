4ce01ec402781f455f9f06a7fc033f8e8e8aa742
Vijaya Malla <mallabiju@gmail.com>
2025-02-18 15:07:36 +0545

    Basic IO

---
+++ B_Basic_IO/b_add_two_nums.java
@@ -0,0 +1,9 @@
+package B_Basic_IO;
+
+public class b_add_two_nums {
+    public static void main(String[] args) {
+        int a = 67, b = 89;
+        int sum = a + b;
+        System.out.println("The sum = " + sum);
+    }
+}
