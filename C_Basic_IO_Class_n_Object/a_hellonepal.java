268464e65c13ce81d5e625091eac31faa609cca1
Vijaya Malla <mallabiju@gmail.com>
2025-02-23 11:20:12 +0545

    ... with control statements

--- C_Basic_IO_Class_n_Object/a_hellonepal.java
+++ C_Basic_IO_Class_n_Object/a_hellonepal.java
@@ -4,6 +4,7 @@ public class a_hellonepal {
     String country = "Nepal";
 
     public static void main(String[] args) {
+        // creating an object
         a_hellonepal obj = new a_hellonepal();
         System.out.println("Hello " + obj.country);
     }
---
+++ C_Basic_IO_Class_n_Object/e_user_input_student_details.java
@@ -0,0 +1,44 @@
+package C_Basic_IO_Class_n_Object;
+
+import java.util.Scanner;
+
+class Studnt {
+    int roll, age;
+    String name;
+
+    void getData(Scanner sc) {
+        roll = sc.nextInt();
+        age = sc.nextInt();
+        name = sc.next();
+    }
+
+    void display() {
+        System.out.println("Roll = " + roll);
+        System.out.println("Age = " + age);
+        System.out.println("Name = " + name);
+    }
+}
+
+public class e_user_input_student_details {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the details of student 1");
+        Studnt s1 = new Studnt();
+        s1.getData(sc);
+        System.out.println("Enter the details of student 2");
+        Studnt s2 = new Studnt();
+        s2.getData(sc);
+        System.out.println("Enter the details of student 3");
+        Studnt s3 = new Studnt();
+        s3.getData(sc);
+
+        System.out.println("Displaying the details of student 1");
+        s1.display();
+        System.out.println("Displaying the details of student 2");
+        s2.display();
+        System.out.println("Displaying the details of student 3");
+        s3.display();
+        sc.close();
+    }
+}
