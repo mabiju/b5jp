71d7e5248027ac42164722e3961d4353255fce6f
Vijaya Malla <mallabiju@gmail.com>
2025-02-26 17:58:32 +0545

    Type casting and lirerals

---
+++ G_Type_Casting/e_string_to_int_calc.java
@@ -0,0 +1,11 @@
+package G_Type_Casting;
+
+public class e_string_to_int_calc {
+    public static void main(String[] args) {
+        int num_val = 456;
+        String string_val = "123";
+        int string_num = Integer.parseInt(string_val);
+        int sum = num_val + string_num;
+        System.out.println("The sum = " + sum);
+    }
+}
---
+++ G_Type_Casting/f_double_to_string.java
@@ -0,0 +1,10 @@
+package G_Type_Casting;
+
+public class f_double_to_string {
+    public static void main(String[] args) {
+        double num_double = 123.4567d;
+        System.out.println("The double value is =" + num_double);
+        String string_double = Double.toString(num_double);
+        System.out.println("The string conversion of double value = " + string_double);
+    }
+}
