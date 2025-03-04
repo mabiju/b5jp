c5c7b1d00e95e58ca4d77be772e837cd674f3d2a
Vijaya Malla <mallabiju@gmail.com>
2025-03-04 14:16:45 +0545

    Command line argument

---
+++ J_Command_Line_Argument/a_command_line.java
@@ -0,0 +1,8 @@
+package J_Command_Line_Argument;
+public class a_command_line {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
+
+// Note : This program must be run using cmd. See Screenshot.
---
+++ J_Command_Line_Argument/b_command_line.java
@@ -0,0 +1,11 @@
+package J_Command_Line_Argument;
+public class b_command_line {
+	public static void main(String[] args) {
+		System.out.println("Sanothimi Campus");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
+
+// Note : This program must be run using cmd. See Screenshot.
