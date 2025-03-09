96ef26912e2a1e3614cc83ea8a8327252344b98c
Vijaya Malla <mallabiju@gmail.com>
2025-03-09 19:24:45 +0545

    Garbage collection, static fields and assignment

---
+++ N_Static_Fields_Method/a_static_variable.java
@@ -0,0 +1,11 @@
+package N_Static_Fields_Method;
+
+public class a_static_variable {
+    static int roll = 56;
+    static String name = "Ram";
+
+    public static void main(String[] args) {
+        System.out.println("Roll = " + roll);
+        System.out.println("Name = " + name);
+    }
+}
