e975ed31578e3441fb3d46fdaf03143ec4a780d4
Vijaya Malla <mallabiju@gmail.com>
2025-03-27 18:27:09 +0545

    access specifier

---
+++ W_Access_Specifier/PackageOne/a_protected_method.java
@@ -0,0 +1,7 @@
+package W_Access_Specifier.PackageOne;
+
+public class a_protected_method {
+    protected void hello(){
+        System.out.println("Hello Sanothimi");
+    }
+}
---
+++ W_Access_Specifier/a_access_specifier_default.java
@@ -0,0 +1,13 @@
+package W_Access_Specifier;
+
+class Nums {
+    // there is not any access specifier keywor used, i.e. default
+    int a = 5;
+}
+
+public class a_access_specifier_default {
+    public static void main(String[] args) {
+        Nums obj = new Nums();
+        System.out.println("The value = " + obj.a);
+    }
+}
