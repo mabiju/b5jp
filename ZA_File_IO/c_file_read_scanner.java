e3642ef65dddf5019c550447e59b4f2fdc326231
Vijaya Malla <mallabiju@gmail.com>
2025-04-08 19:32:42 +0545

    File IO

---
+++ ZA_File_IO/c_file_read_scanner.java
@@ -0,0 +1,17 @@
+package ZA_File_IO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class c_file_read_scanner {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("D:/SCJPB5/ZA_File_IO/ram.txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+}
