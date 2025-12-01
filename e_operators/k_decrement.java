9a0f278f4e52666c79936793dd06d1eb944b0cf4
Vijaya Malla <mallabiju@gmail.com>
2025-12-01 12:09:53 +0545

    Operators

---
+++ e_operators/k_decrement.java
@@ -0,0 +1,9 @@
+package e_operators;
+
+public class k_decrement {
+    public static void main(String[] args) {
+        int a = 5;
+        // System.out.println(a--); // Post-decrement // 5
+        System.out.println(--a); // Pre-decrement // 4
+    }
+}
