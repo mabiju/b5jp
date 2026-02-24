529ad8606b8c75db485d4fb5689b8f4f042090e2
Vijaya Malla <mallabiju@gmail.com>
2026-02-24 08:38:50 +0545

    Thread

---
+++ y_threads/i_thread_life_cycle.java
@@ -0,0 +1,58 @@
+package y_threads;
+
+
+class ThreadOne extends Thread{
+    public void run()
+    {
+        System.out.println("Thread One");
+        System.out.println("Execution of i in ThreadOne");
+
+        for (int i = 1; i <=5; i++) {
+            System.out.println("i in ThreadOne: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("ThreadOne execution is terminated");
+    }
+}
+class ThreadTwo extends Thread{
+    public void run()
+    {
+        System.out.println("Thread Two");
+        System.out.println("Execution of i in ThreadTwo");
+
+        for (int i = 1; i <=5; i++) {
+            System.out.println("i in ThreadTwo: " + i);
+        }
+        System.out.println("ThreadTwo execution is terminated");
+    }
+}
+
+public class i_thread_life_cycle {
+    public static void main(String[] args) {
+        // life cycle of thread
+        // Threads new state
+        ThreadOne t1 = new ThreadOne();
+        ThreadTwo t2 = new ThreadTwo();
+        // both the above threads are in runnable state
+        // running state of both threads
+        t1.start();
+        // move control to another thread
+        // A yield() method is a static method of Thread class
+        // and it can stop the currently executing thread and will
+        // give a chance to other waiting threads of the same priority.
+        Thread.yield();
+        // blocked state ThreadOne
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+        System.out.println("Main thread is completed here.");
+    }
+}
---
+++ y_threads/j_thread_life_cycle.java
@@ -0,0 +1,41 @@
+package y_threads;
+
+class ThreadLife extends Thread {
+    private String threadName;
+
+    ThreadLife(String name) {
+        threadName = name;
+        System.out.println("Thread: " + threadName + " is in NEW state.");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread: " + threadName + " is in RUNNING state.");
+        try {
+            for (int i = 3; i > 0; i--) {
+                System.out.println("Thread: " + threadName + " is performing task " + i);
+                System.out.println("Thread: " + threadName + " is in WAITING state.");
+                Thread.sleep(1000); // Simulates waiting state
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread: " + threadName + " was interrupted.");
+        }
+        System.out.println("Thread: " + threadName + " is in TERMINATED state.");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Thread: " + threadName + " is in RUNNABLE state.");
+        super.start();
+    }
+}
+
+public class j_thread_life_cycle {
+    public static void main(String[] args) {
+        ThreadLife t1 = new ThreadLife("Thread-1");
+        ThreadLife t2 = new ThreadLife("Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
