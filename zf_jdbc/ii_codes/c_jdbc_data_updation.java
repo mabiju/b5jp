b3e4597adbfa6cf1c8e47c7dc4c969ecded2de9c
Vijaya Malla <mallabiju@gmail.com>
2026-03-22 20:26:47 +0545

    jdbc final updates

---
+++ zf_jdbc/ii_codes/c_jdbc_data_updation.java
@@ -0,0 +1,32 @@
+package zf_jdbc.ii_codes;
+
+// Java program to illustrate updating the Database
+import java.sql.*;
+
+// public class App {   // use this in real scenario
+public class c_jdbc_data_updation {
+    public static void main(String[] args) throws Exception {
+        String userid = "ram";
+        String userpwd = "ram@123";
+        String newpwd = "@123ram";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establishing connetion
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_jdbc", "root", "");
+
+            Statement stmt = con.createStatement();
+
+            // updating data into database table
+            String query = "update users set pwd ='"+newpwd +"' where id = '"+userid+"' and pwd = '"+userpwd+"' ";
+            int a = stmt.executeUpdate(query);
+            if (a > 0) {
+                System.out.println("Data updated successfully.");
+            } else {
+                System.out.println("Something went wrong....");
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+    }
+}
